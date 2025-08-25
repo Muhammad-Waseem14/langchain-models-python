85b1c7efe8a72ba72ae1ed5c8f6655d3626240d6
Muhammad-Waseem14 <muhammad.wasim@arbisoft.com>
2025-08-26 00:38:36 +0500

    added comment defining prompt template

--- chatModels/prompt_template.py
+++ chatModels/prompt_template.py
@@ -10,6 +10,13 @@ model = ChatOpenAI(model='gpt-4o-mini', max_completion_tokens=50)
 
 system_template = "Translate the following from English into {language}"
 
+# A PromptTemplate is like a blueprint for your AI questions.
+# Instead of writing the same full prompt again and again, you create a template with placeholders.
+# Then you just fill in the blanks when you want to ask the model something.
+# Example:
+# Template:
+# Translate this text into {language}: {text}
+
 prompt_template = ChatPromptTemplate.from_messages(
     [("system", system_template), ("user", "{text}")]
 )
